7dbd9bc15a450624bca7067d64ab98e114d12b2c
yamato-1923 <kuwayama080@gmail.com>
2024-05-26 02:22:19 +0000

    init

---
+++ prisma/migrations/20240526021810_init/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
